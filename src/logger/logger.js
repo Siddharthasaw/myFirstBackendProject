645bc32d4e9a7e8cf08f68e4c126f90e3fee71cd
Siddharthasaw <sawsiddhartha@gmail.com>
2022-10-15 16:05:18 +0530

    this is my assignment

---
+++ src/logger/logger.js
@@ -0,0 +1,7 @@
+
+let welcome = function() {
+    console.log("Welcome to my application, I m siddhartha and a part of functionup Lithium cohort")
+}
+
+
+module.exports.myFunction = welcome
---
+++ src/validator/formatter.js
@@ -0,0 +1,9 @@
+function problem()
+{
+    let str = "  Siddhartha Saw  ";
+    console.log("after trim "+str.trim());
+    console.log("converting into lowercase-"+str.toLowerCase())
+    console.log("convserting into uppercase-"+str.toUpperCase())
+}
+
+module.exports.trims = problem
